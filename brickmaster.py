0ec672fe17a4edee27fbe5371435bea9e8578a2c
chrisgilldc <chris@chrisgill.net>
2021-04-04 23:20:00 +0100

    Separate library functions and flask app.

--- brickmaster.py
+++ brickmaster.py
@@ -3,6 +3,9 @@
 # Brickmaster - a Flask App for Lego Control!
 
 # Bring in libraries
+#
+from lib.libbrickmaster import brickmaster
+
 ## Core Components
 from flask import Flask, request, abort, jsonify
 from flask_restful import Resource, Api, reqparse
@@ -21,108 +24,22 @@ brickmaster_config = {
 	}
 }
 
-# Conditional library loading.
-found_gpio = False
-found_8relay = False
-found_insession = False
-for key in brickmaster_config['controls'].keys():
-	if ( brickmaster_config['controls'][key]['type'] == 'GPIO' ) & ( not found_gpio ):
-		import RPi.GPIO as GPIO
-		## Set Board numbering system
-		GPIO.setmode(GPIO.BCM)
-		# Set up the Channels
-		for key, control in brickmaster_config['controls'].items():
-			if brickmaster_config['controls'][key]['type'] == "GPIO":
-				GPIO.setup(control['pin'], GPIO.OUT)
-		# Only process once.
-		found_gpio = True
-	if ( brickmaster_config['controls'][key]['type'] == '8relay' ) & ( not found_8relay ):
-		import lib8relay
-		# Create the 8Relay library
-		l8 = lib8relay
-		# Only process once.
-		found_8relay = True
-	if ( brickmaster_config['controls'][key]['automate'] == 'insession' ) & ( not found_insession ):
-		# Bring in insession and create object instance.
-		from insession import insession
-		insession = insession()
-		# Call statuses to trigger a calendar load.
-		insession.status('house')
-		insession.status('senate')
-
-		# Need an automatic scheduler to take action based on the calendar
-		from apscheduler.schedulers.background import BackgroundScheduler
-		clerk = BackgroundScheduler()
-
-	if found_gpio & found_8relay:
-		# If we've found all known output types, break early.
-		break
-
-
 # Create the Flask app
 app = Flask(__name__)
 api = Api(app)
 
+# Create the Brickmaster instance. Takes the controls, 
+bm = brickmaster(brickmaster_config['controls'])
 
-# Define the Brickmaster class
-class Brickmaster(Resource):
-	# Tool to validate config at start. Going to write it...some day.
-	#def validate_config(self,config):
-		# Things!
-
-	# Helper function to validate control info.
-	def check_control(self,control):
-		# Does the control exist?
-		if not control in brickmaster_config['controls']:
-			abort(406,'Requested control (' + str(control) + ') not configured')
-		return 0
-
-	# Helper function to turn booleans into text
-	def bool_to_text(self,value):
-		if int(value) == 0:
-			text_value = "Off"
-		elif int(value) == 1:
-			text_value = "On"
-		else:
-			abort(500,'Passed meaningless value')
-		return text_value
-
-	# Function for getting status of a given control. This is separate from "Get" because it's needed elsewhere, beyond just Get.
-	def control_status(self,control):
-		# Set up return dict and seed with the name of the requested control
-		return_status = {}
-		return_status['control'] = control
-
-		# Fetch status in the appropriate way
-		if brickmaster_config['controls'][control]['type'] == 'GPIO':
-			# Retrieve the LED control object...
-			try:
-				control_status = GPIO.input(brickmaster_config['controls'][control]['pin'])
-			except:
-				abort(503,'Failed to get GPIO status.')
-			return_status['status'] = self.bool_to_text(control_status)
-			return_status['is_on'] = bool(control_status)
-
-		elif brickmaster_config['controls'][control]['type'] == '8relay':
-			try:
-				control_status = l8.get(brickmaster_config['controls'][control]['stack'],brickmaster_config['controls'][control]['relay'])
-			except:
-				abort(503,'Relay board failed to get status. Please check hardware.')
-
-			# Return both text version and boolean version of status. This is needed for Home Assistant, at least.
-			return_status['status'] = self.bool_to_text(control_status)
-			return_status['is_on'] = bool(control_status)
-		else:
-			abort(500,'Unsupported control type encountered.')
-
-		return return_status
-
+# Create Brickmaster Flask Interface
+class bmfi(Resource):
 	def get(self,control):
 		# Confirm we have a valid control request.
-		self.check_control(control)
-
-		# Return status in a nice JSON format.
-		return jsonify(self.control_status(control))
+		if bm.check_control(control):
+			abort(406,"Control '" + control + "' does not exist.")
+		else:
+			# Return status in a nice JSON format.
+			return jsonify(bm.control_status(control))
 
 	# Posting for on/off
 	def post(self,control=None):
@@ -133,37 +50,13 @@ class Brickmaster(Resource):
 		return_status=[]
 
 		# Get posted data.
-		input_controls=request.get_json()
+		input_controls = request.get_json()
 
 		for control in input_controls.keys():
-			# Make sure it exists
-			self.check_control(control)
+			print("Setting state for: " + control)
+			bm.control_set(control,input_controls[control])
 
-			# Only an affirmative "On" gets an attempt to set up.
-			# Otherwise, turn it off because something's wonky
-			if input_controls[control].lower() == 'on':
-				control_val = 1
-			else:
-				control_val = 0
-
-			# Perform the correct action for the control type
-			if brickmaster_config['controls'][control]['type'] == 'GPIO':
-				# Set the GPIO pin status
-				if control_val:
-					GPIO.output(brickmaster_config['controls'][control]['pin'],control_val)
-				else:
-					GPIO.output(brickmaster_config['controls'][control]['pin'],0)
-			elif brickmaster_config['controls'][control]['type'] == '8relay':
-				# Make the call to the relay board
-				try:
-					l8.set(brickmaster_config['controls'][control]['stack'],brickmaster_config['controls'][control]['relay'],control_val)
-				except:
-					abort(503,'Relay board failed to set status. Please check hardware.')
-			else:
-				abort(500,'Unsupported control type encountered')
-
-			# Get the new status and push it into the return array.
-			return_status.append(self.control_status(control))
+			return_status.append(bm.control_status(control))
 
 		return jsonify(return_status)
 
@@ -172,11 +65,7 @@ class Brickmaster(Resource):
 
 # Set up all the resources
 
-api.add_resource(Brickmaster, '/brickmaster', '/brickmaster/<string:control>')
+api.add_resource(bmfi, '/brickmaster', '/brickmaster/<string:control>')
 
 if __name__ == '__main__':
 	app.run(host=brickmaster_config['host'],port=brickmaster_config['port'])
-
-
-# Cleanup when exiting
-GPIO.cleanup()
