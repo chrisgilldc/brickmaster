86b44f4060f9b4ebc9a10b8ecd96287c227c7fc3
Chris Gill <chris@chrisgill.net>
2021-07-01 07:18:55 -0400

    Separage agent for Congressional status

---
+++ agents/insession.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+###
+#
+# InSession
+#
+#
+# A Brickmaster agent to control lights in the Capitol (21030) based on actual status of Congress.
+#
+###
+
+## https://learn.adafruit.com/matrix-7-segment-led-backpack-with-the-raspberry-pi
