bff71c86c67afa2ecc695a5a5dac0963bd638382
Chris Gill <chris@chrisgill.net>
2021-07-02 11:19:17 -0400

    Cleanup push.

--- agents/format_7s.py
+++ agents/format_7s.py
@@ -5,22 +5,34 @@
 ###
 
 
-def time_7s(data,elements):
+def time_7s(data):
+	# Return an error if this is out of range.
+	if data <= -599 or data > 6039:
+		return("ERR")
+	# Negative values under 60s.
+	elif data < 59:
+		data = abs(data)
+		return("-" + str(int(data // 60)) + ":" + str(int(data % 60)).zfill(2))
 	# Over 60s, M:S
-	if data >= 60:
+	elif data >= 60:
 		return(str(int(data // 60)).zfill(2) + ":" + str(int(data % 60)).zfill(2))
 	# Otherwise, 00:SS
 	else:
 		return("00:" + str(int(data)).zfill(2))
 
-def number_7s(data,element):
-	if len(str(int(data))) >= 4:
-		return str(int(data))
-	elif len(str(int(data))) == 3:
-		return str(int(data)) + "." + str((data/1) - (data//1))[0]
-	elif len(str(int(data))) == 2:
-		return str(int(data)) + "." + str((data/1) - (data//1))[0-1]
-	elif len(str(int(data))) == 1:
-		return str(int(data)) + "." + str((data/1) - (data//1))[0-3]
+def number_7s(data):
+	data = str(data).split('.')
+	# Return error if integer part is too long to display.
+	if len(data[0]) > 4:
+		return("E-TL")
+	# If it's not a float, just return the value back.
+	elif len(data) == 1:
+		return(str(data[0]))
 	else:
-		return data
+		# Have we gotten to >= 1000? Then just return that.
+		if int(data[0]) >= 1000:
+			return(str(data[0]))
+		else:
+		# Otherwise, include as many decimals as we can.
+			float_len = 4 - len(data[0])
+			return(str(data[0] + '.' + data[1][0:float_len]))
--- agents/launchdirector.py
+++ agents/launchdirector.py
@@ -9,12 +9,14 @@
 ####
 
 
-# Signal handler
+stage_file = "saturn5.json"
+
+# Basic libraries
+import sys
 import signal
-# Basic scheduler
 import sched, time
-# Datetime conversion
 from datetime import timedelta
+import json
 
 # Quick 7s formatting library
 from format_7s import time_7s, number_7s
@@ -22,6 +24,10 @@ from format_7s import time_7s, number_7s
 # Support for 7-segment displays via Adafruit backpacks.
 import board
 from adafruit_ht16k33.segments import Seg7x4, BigSeg7x4
+
+# Pretty Printing, for debugging.
+import pprint
+pp = pprint.PrettyPrinter(depth=4)
 
 # Set up i2c communication
 i2c = board.I2C()
@@ -34,35 +40,26 @@ mc_clock.print("00:00")
 mc_alt.print("0000")
 mc_speed.print("0000")
 
+def launch():
+	next_time = time.time() + 1
+	fst = 1
+	while fst <= len(flight_data):
+		time.sleep(max(0,next_time - time.time()))
+		update_flight(fst)
+		fst = fst + 1
+		next_time += (time.time() - next_time) // 1 * 1 + 1
 
-def every(delay, task):
-	next_time = time.time() + delay
-	while True:
-		time.sleep(max(0, next_time - time.time()))
-		task()
-		next_time += (time.time() - next_time) // delay * delay + delay
-
-def mc_update():
-	# Set elapsed clock
-	elapsed_time = round(time.time() - launch_time,3)
-
-	# Calculated altitude
-	derived_altitude = elapsed_time * 10
-
-	# calculated speed
-	derived_speed = elapsed_time * 5
-
-	display_time = time_7s(elapsed_time,4)
-	display_alt = number_7s(derived_altitude,4)
-	display_speed = number_7s(derived_speed,4)
+def update_flight(fst):
+	# We use 'flight sim time' (fst) to distinguish from actual mission elapsed time
+	current_state = flight_data[fst]
 
 	# Print to log
-	print(display_time + "\t" + display_alt + "\t" + display_speed)
+	print(str(current_state['t']) + "\t" + str(current_state['a']) + "\t" + str(current_state['v']) + "\t" + str(current_state['s']))
 
 	# Push to displays
-	mc_clock.print(display_time)
-	mc_alt.print(display_alt)
-	mc_speed.print(display_speed)
+	mc_clock.print(current_state['t'])
+	mc_alt.print(current_state['a'])
+	mc_speed.print(current_state['v'])
 
 # Function to shut everything down.
 def all_off():
@@ -76,6 +73,92 @@ def keyboardInterruptHandler(signal, frame):
 
 signal.signal(signal.SIGINT,keyboardInterruptHandler)
 
-print("Clock\tAlt\tSpeed")
+def read_flight_data(stage_file):
+
+	stage_handle = open(stage_file)
+	stage_json = json.load(stage_handle)
+	stage_handle.close()
+	# Calculate out the stage!
+	flight_data = {}
+	flight_sequence = 1
+	# Set up the pre-roll
+	pre_roll = int(stage_json['pre-roll']) * -1
+	while pre_roll < 0:
+		if pre_roll > stage_json['ignition']:
+			flight_data[flight_sequence] = { "t": time_7s(pre_roll), "s": 1, "a": number_7s(0), "v": number_7s(0), "gpio": stage_json['stages']['1']['gpio']}
+		else:
+			flight_data[flight_sequence] = { "t": time_7s(pre_roll), "s": 0, "a": number_7s(0), "v": number_7s(0), "gpio": 0 }
+		pre_roll = pre_roll + 1
+		flight_sequence = flight_sequence + 1
+
+	flight_data[flight_sequence] = { "t": time_7s(0), "s": 1, "a": number_7s(0), "v": number_7s(0), "gpio": stage_json['stages']['1']['gpio'] }
+	# Save the Zero-Mark for later calculations
+	zero_mark = flight_sequence
+
+	# Set up staging variables variables
+	working_stage = 1
+	# Add in each stage in sequence.
+	while working_stage <= len(stage_json['stages']):
+
+		# 1st stage has no previous stage
+		if working_stage == 1:
+			burn_time = stage_json['stages'][str(working_stage)]['burn_time'] + stage_json['ignition']
+			dV = stage_json['stages'][str(working_stage)]['final_velocity'] / burn_time
+			dA = stage_json['stages'][str(working_stage)]['final_alt'] / burn_time
+		else:
+			burn_time = stage_json['stages'][str(working_stage)]['burn_time']
+			# Find the ending data from the previous stage.
+			previous = max(flight_data)
+			dV = (stage_json['stages'][str(working_stage)]['final_velocity'] - float(flight_data[previous]['v']) )/ burn_time
+			dA = (stage_json['stages'][str(working_stage)]['final_alt'] - float(flight_data[previous]['a']) ) / burn_time
+		target_time = flight_sequence + int(stage_json['stages'][str(working_stage)]['burn_time'])
+		while flight_sequence <= target_time:
+			flight_data[flight_sequence] = {
+				"t": time_7s(flight_sequence-zero_mark),
+				"s": working_stage,
+				"a": number_7s(float(flight_data[flight_sequence-1]['a']) + dA),
+				"v": number_7s(float(flight_data[flight_sequence-1]['v']) + dV)
+			}
+			flight_sequence = flight_sequence + 1
+
+		# Process an inter-stage pause.
+		if stage_json['stages'][str(working_stage)]['interstage']:
+			# During inter-stage, altitude and time increase, but velocity doesn't.
+			target_time = flight_sequence + int(stage_json['stages'][str(working_stage)]['interstage'])
+			while flight_sequence <= target_time:
+				flight_data[flight_sequence] = {
+					"t": time_7s(flight_sequence - zero_mark),
+					"s": working_stage,
+					"a": number_7s(float(flight_data[flight_sequence-1]['a']) + dA),
+					"v": number_7s(float(flight_data[flight_sequence-1]['v']))
+				}
+				flight_sequence = flight_sequence + 1
+
+		working_stage = working_stage + 1
+
+	# Add in the post-roll
+	if stage_json['post-roll']:
+		target_time = flight_sequence + stage_json['post-roll']
+		while flight_sequence <= target_time:
+			flight_data[flight_sequence] = {
+				# Freeze all values.
+				"t": flight_data[flight_sequence-1]['t'],
+				"s": "Post-roll",
+				"a": flight_data[flight_sequence-1]['a'],
+				"v": flight_data[flight_sequence-1]['v']
+			}
+			flight_sequence = flight_sequence + 1
+
+	return(flight_data)
+
+# Flight data prep!
+flight_data = read_flight_data('saturn5.json')
+
+print("Flight plan loaded.")
+pp.pprint(flight_data)
+print("")
+
+print("Clock\tAlt\tSpeed\tStage")
 launch_time = time.time()
-every(1,mc_update)
+launch()
+all_off()
