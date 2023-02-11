beb41f416a46babb9f933d36e40ce5f20d74b7f5
Chris <chris@chrisgill.net>
2023-02-11 17:51:47 -0500

    Initial Development Studs

--- tools/display_test.py
+++ tools/display_test.py
@@ -13,4 +13,6 @@ parser.add_argument('-a', '--address', nargs='+')
 args = parser.parse_args()
 
 for i2c_addr in args.address:
+    # Convert to Base 16 integer
+    i2c_addr = int(i2c_addr, 16)
     print(hex(i2c_addr))
