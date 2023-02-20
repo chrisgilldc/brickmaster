19d2e7f1816589bbd843a520fb0efc1d0b9f57c9
Chris <chris@chrisgill.net>
2023-02-20 08:28:55 -0500

    Set up I2C Bus on startup. Set up displays on startup. Call display idle method during idle run loop. Turn off displays on shutdown.

--- brickmaster2/brickmaster2.py
+++ brickmaster2/brickmaster2.py
@@ -4,7 +4,11 @@ import adafruit_logging as logging
 import atexit
 from .config import BM2Config
 from .controls import CtrlGPIO
+from .display import Display
 from .network import BM2Network
+import board
+import busio
+from datetime import datetime
 
 class BrickMaster2:
     def __init__(self, cmd_opts=None):
@@ -23,11 +27,18 @@ class BrickMaster2:
         # Create the config processor
         self._bm2config = BM2Config()
 
-        # Set up the network.
-        self._network = BM2Network(self._bm2config.network_config)
+        # Setup the I2C Bus.
+        self._setup_i2c_bus(None)
 
-        # Dict to store control objects.
+        # Set up the network.
+        self._network = BM2Network(self._bm2config.system)
+
+        # Initiatlize dicts to store objects.
         self._controls = {}
+        self._displays = {}
+        # Lists for displays that show the time or date.
+        self._clocks = []
+        self._dates = []
 
         # Set up the controls.
         for control_cfg in self._bm2config.controls:
@@ -36,6 +47,16 @@ class BrickMaster2:
                 self._controls[control_cfg['name']] = CtrlGPIO(control_cfg)
 
         self._logger.debug("Have controls: {}".format(self._controls))
+
+        # Set up the displays.
+        for display_cfg in self._bm2config.displays:
+            self._logger.info("Setting up display '{}'".format(display_cfg['name']))
+            self._displays[display_cfg['name']] = Display(display_cfg, self._i2c_bus, )
+            if display_cfg['when_idle'] == 'time':
+                self._clocks.append(display_cfg['name'])
+            elif display_cfg['when_idle'] == 'date':
+                self._dates.append(display_cfg['name'])
+
 
         # Pass the controls to the Network module.
         for control_name in self._controls:
@@ -46,6 +67,12 @@ class BrickMaster2:
         while True:
             # Poll the network.
             self._network.poll()
+            # Push time and date to displays that need it.
+            for display in self._displays:
+                self._displays[display].show_idle()
+
+    def _setup_i2c_bus(self, i2c_config):
+        self._i2c_bus = busio.I2C(board.SCL, board.SDA)
 
     def _print_or_log(self, level, message):
         try:
@@ -62,6 +89,11 @@ class BrickMaster2:
         for control in self._controls:
             self._print_or_log("info", "\t{}".format(control))
             self._controls[control].set("off")
+        # Turn off all the displays
+        self._print_or_log("critical", "Setting displays off....")
+        for display in self._displays:
+            self._print_or_log("info", "\t{}".format(display))
+            self._displays[display].off()
         # Poll the network one more time to ensure the new control status is sent.
         self._network.poll()
         # Send an offline message.
