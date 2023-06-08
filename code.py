40687a1bab908776ae1b3a1e9bcb7703c5ebb35e
Chris <chris@chrisgill.net>
2023-06-08 10:03:45 -0400

    Auto-reload if system throws a memory error.

---
+++ code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# BrickMaster2 Executor for CircuitPython Boards
+
+from brickmaster2.brickmaster2 import BrickMaster2
+import time
+import supervisor
+
+try:
+    # Create the BrickMaster2 Object.
+    bm2 = BrickMaster2(None)
+    # Run it.
+    bm2.run()
+except MemoryError as e:
+    print(e)
+    print("Waiting for 10s before soft reset.")
+    time.sleep(10)
+    supervisor.reload()
