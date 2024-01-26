1ba7bcfff14241193111bff9a34047642434c965
Chris <chris@chrisgill.net>
2024-01-26 12:34:41 -0500

    Updating CLI for Linux invocation and pip installation.

--- brickmaster2/__init__.py
+++ brickmaster2/__init__.py
@@ -1,2 +1,5 @@
 # BrickMaster2 Package
+
+from brickmaster2.brickmaster2 import BrickMaster2
+
 from .version import __version__
--- brickmaster2/cli/brickmaster2.py
+++ brickmaster2/cli/brickmaster2.py
@@ -1,12 +1,49 @@
 #!/usr/bin/python3
 # Brickmaster2 Command Executor.
-from brickmaster2.brickmaster2 import BrickMaster2
+import brickmaster2
+import argparse
+from pathlib import Path
+from pid import PidFile
+import pwd
+import os
+import sys
 
 # Do some argument parsing here.
 cmd_opts = None
 
-# Create the BrickMaster2 Object.
-bm2 = BrickMaster2(cmd_opts)
-# Connect interrupt and terminate signal handlers to the clean shutdown method.
-# Run it.
-bm2.run()
+def main():
+    print("CobraBay Parking System - {}".format(brickmaster2.__version__))
+    print("Running as '{}'".format(pwd.getpwuid(os.getuid()).pw_name))
+    # Parse command line options.
+    parser = argparse.ArgumentParser(
+        description="CobraBay Parking System"
+    )
+    parser.add_argument("-c", "--config", default="./config.json", help="Config file location.")
+    parser.add_argument("-r", "--rundir", default="/tmp", help="Run directory, for the PID file.")
+    args = parser.parse_args()
+
+    config_path = Path(args.config)
+    if not config_path.is_absolute():
+        print("Config file path is not absolute. Presuming it's relative to current directory.")
+        config_path = Path.cwd() / config_path
+    if not config_path.is_file():
+        print("Config file '{}' does not exist! Cannot continue!")
+        sys.exit(1)
+
+    print("Using config file '{}'".format(config_path))
+
+    # Start the main operating loop.
+    try:
+        with PidFile('CobraBay_' + config_path.stem, piddir=args.rundir) as p:
+            print("Running as PID {}".format(p.pid))
+
+            # Initialize the system
+            print("Initializing...")
+            bm2 = brickmaster2.BrickMaster2(config_file=str(config_path))
+            # Start.
+            print("Initialization complete. Operation start.")
+            bm2.run()
+
+    except pid.base.PidFileAlreadyLockedError:
+        print("Cannot start, already running!")
+
