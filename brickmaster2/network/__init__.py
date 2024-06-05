6c50919edacbd0acb347bc703ad53725f25b9b48
Chris <chris@chrisgill.net>
2024-06-05 08:22:03 -0400

    Interim commit of network reorg.

--- brickmaster2/network/__init__.py
+++ brickmaster2/network/__init__.py
@@ -3,6 +3,10 @@ import sys
 # Import common MQTT methods.
 from . import mqtt
 
+# Import the base Network class
+# from .base import BM2Network
+# from . import base
+
 # Conditionally import the correct version of the module.
 if sys.implementation.name == 'cpython':
     from .linux import BM2NetworkLinux
