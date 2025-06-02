815a326a6e0c39208b8880095d623d08931f7146
Chris <chris@chrisgill.net>
2025-06-02 08:46:51 -0400

    Logger fix.

--- brickmaster/controls/CtrlNull.py
+++ brickmaster/controls/CtrlNull.py
@@ -11,8 +11,20 @@ class CtrlNull(BaseControl):
     """
     Null control class. When we need a control to exist but not do anything.
     """
-    def __init__(self, ctrl_id, name, core):
-        super().__init__(ctrl_id, name, core)
+    def __init__(self, ctrl_id, name, core, logger):
+        """
+        Null control. Goes Nowhere, Does Nothing.
+
+        @param ctrl_id: Short ID for the control. No spaces!
+        @type ctrl_id: str
+        @param name: Long name for the control
+        @type name: str
+        @param core: Reference to the Brickmaster core object.
+        @type core: object
+        @param logger: Logger object.
+        @type logger: adafruit_logging.Logger
+        """
+        super().__init__(ctrl_id, name, core, logger)
 
     def set(self, value):
         self._logger.debug(f"Null control set to '{value}'")
